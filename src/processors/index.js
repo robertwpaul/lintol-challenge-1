bbebc8388408ae31c12a85f75d7a929a947169d4
Kyle Harrison <kyle90adam@hotmail.com>
2018-04-13 21:05:33 +0100

    Adding MAC address processor

--- src/processors/index.js
+++ src/processors/index.js
@@ -2,12 +2,14 @@ const ip = require('./ip');
 const phoneNumber = require('./phone-number');
 const nationalInsuranceNumber = require('./national-insurance');
 const email = require('./email');
+const macAddress = require('./mac-address');
 
 module.exports = {
   textProcessors: [
     ip,
     phoneNumber,
     nationalInsuranceNumber,
-    email
+    email,
+    macAddress
   ]
 };
